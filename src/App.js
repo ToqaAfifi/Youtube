54ba9489829408d731c479fb04a94319a1c9fef2
Toqa Afifi <toqa.muhammad.afifi@gmail.com>
2023-01-23 22:45:38 +0200

    Done with the header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      
+    <div className="root">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import classes from './Header.module.scss';
+
+const Header = () => {
+
+    const [layout, setLayout] = useState('desktop');
+
+    useEffect(() => {
+
+        window.addEventListener("resize", () => {
+            if (window.innerWidth <= 600) {
+                setLayout("mobile");
+            }
+            else {
+                setLayout("desktop");
+            }
+        });
+
+        return () => {
+            window.removeEventListener("resize", () => {
+                if (window.innerWidth <= 600) {
+                    setLayout("mobile");
+                }
+                else {
+                    setLayout("desktop");
+                }
+            });
+        };
+    }, [])
+
+    return (
+        <header>
+            {
+                layout === 'desktop' ?
+                    <img src={`${process.env.PUBLIC_URL}/images/youtubeLogo.svg`} alt="logo" className={classes.logo} /> : <img src={`${process.env.PUBLIC_URL}/images/youtubeMinLogo.svg`} alt="logo" className={classes.logo} />
+            }
+        </header>
+    )
+}
+
+export default Header;
